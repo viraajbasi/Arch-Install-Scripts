04188e88c8e9d139dd9ada0f1b42e6bfc85cf9dd
Viraaj Basi <viraajbasi@gmail.com>
2021-08-28 18:05:44 +0100

    removed windows fonts

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ useradd -mG wheel viraaj
 echo "viraaj:password" | chpasswd
 
 sed -i "93,94s/#//;37s/#//" /etc/pacman.conf
-pacman -Syu --needed --noconfirm base-devel fish networkmanager bluez bluez-utils efibootmgr dosfstools mtools ntfs-3g xdg-user-dirs pipewire pipewire-pulse pipewire-jack pipewire-alsa htop neofetch ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra unzip firewalld openssh
+pacman -Syu --needed --noconfirm base-devel fish networkmanager bluez bluez-utils efibootmgr dosfstools mtools ntfs-3g xdg-user-dirs pipewire pipewire-pulse pipewire-jack pipewire-alsa htop neofetch ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra p7zip firewalld openssh
 
 systemctl enable NetworkManager.service bluetooth.service firewalld.service sshd.service
 
--- userinstall.sh
+++ userinstall.sh
@@ -19,16 +19,11 @@ cd uk-intl-kb
 sudo cat uk-intl-kb >> /usr/share/X11/xkb/symbols/gb
 cd ..
 
-curl -L https://github.com/ViraajB/Arch-Install-Scripts/releases/download/1.0/WindowsFonts.zip > WindowsFonts.zip
-mkdir WindowsFonts
-unzip WindowsFonts.zip -d WindowsFonts
-rm -rf WindowsFonts.zip
-sudo mv WindowsFonts /usr/share/fonts
-chmod 644 /usr/share/fonts/WindowsFonts/*
-fc-cache --force
-
 yay -S --needed --noconfirm minigalaxy itch openrgb ttf-google-sans code-features code-icons code-marketplace
 ## If gnome is used instead of plasma
 ## comment the last line
 # yay -S --needed --noconfirm chrome-gnome-shell yaru-gtk-theme yaru-gnome-shell-theme-git gnome-shell-extension-dash-to-dock-git
-echo -e "gtk-modules=\"appmenu-gtk-module\"" | tee -a ~/.gtkrc-2.0 ~/.config/gtk-3.0/settings.ini
+
+mkdir ~/.config/gtk-3.0
+echo -e "gtk-modules=\"appmenu-gtk-module\"" >> ~/.gtkrc-2.0 
+echo -e "[Settings]\ngtk-modules=\"appmenu-gtk-module\"" >> ~/.config/gtk-3.0/settings.ini
