adcd114483e96f911ec28550fc319edb54992656
Viraaj Basi <viraajbasi@gmail.com>
2021-08-31 00:41:05 +0100

    updated nvidia instructions and set the root option for systemd-boot to use the partition uuid

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Variables
+# Change these to match system
+ROOTPART="/dev/sda2"
+ROOTPASSWD="password"
+USERPASSWD="password"
+
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 hwclock --systohc
 
@@ -13,20 +19,26 @@ echo -e "127.0.1.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tarch.localdomain\tar
 
 bootctl --path=/boot install
 sed -i "s/default .*/default arch-*/" /boot/loader/loader.conf
-echo -e "title\tArch Linux\nlinux\t/vmlinuz-linux\ninitrd\t/initramfs-linux.img\noptions\troot=/dev/sda2 rw" >> /boot/loader/entries/arch.conf
+echo -e "title\tArch Linux\nlinux\t/vmlinuz-linux\ninitrd\tamd-ucode.img\ninitrd\t/initramfs-linux.img" >> /boot/loader/entries/arch.conf
+## Comment below if using NVIDIA
+echo -e "options\troot=PARTUUID=$(blkid -s PARTUUID -o value $ROOTPART) rw" >> /boot/loader/entries/arch.conf
 
-echo "root:password" | chpasswd
+echo "root:$ROOTPASSWD" | chpasswd
 sed -i "82s/# //" /etc/sudoers
 useradd -mG wheel viraaj
-echo "viraaj:password" | chpasswd
+echo "viraaj:$USERPASSWD" | chpasswd
 
 sed -i "93,94s/#//;37s/#//" /etc/pacman.conf
 pacman -Syu --needed --noconfirm base-devel zsh grml-zsh-config networkmanager bluez bluez-utils efibootmgr dosfstools mtools ntfs-3g xdg-user-dirs pipewire pipewire-pulse pipewire-jack pipewire-alsa htop neofetch ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra p7zip firewalld openssh
 
-systemctl enable NetworkManager.service bluetooth.service firewalld.service sshd.service
+systemctl enable NetworkManager.service bluetooth.service firewalld.service sshd.service 
 
 ## Nvidia drivers
 # pacman -S --noconfirm nvidia nvidia-utils lib32-nvidia-utils nvidia-settings
+# sed -i "s/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/" /etc/mkinitcpio.conf
+# echo -e "options\troot=PARTUUID=$(blkid -s PARTUUID -o value $ROOTPART) rw nvidia-drm.modeset=1"
+# mkdir -p /etc/pacman.d/hooks
+# echo -e "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=linux\n\n[Action]\nDescription=Update Nvidia module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTarget\nExec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -p'" >> /etc/pacman.d/hooks/nvidia
 
 ## Utility programs
 # pacman -S --noconfirm firefox chromium libreoffice gimp discord cups hplip piper
