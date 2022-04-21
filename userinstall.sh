3a946d3e5d69a4b2ef574b10bb1f5edfc14e2653
Viraaj Basi <viraajbasi@gmail.com>
2022-04-22 00:30:59 +0100

    Replaced minecraft launcher

--- userinstall.sh
+++ userinstall.sh
@@ -13,7 +13,7 @@ mv config.fish $HOME/.config/fish/config.fish
 ## Environment variables for wayland
 mkdir -p $HOME/.config/environment.d/
 echo -e "QT_QPA_PLATFORM=\"wayland;xcb\"\nCLUTTER_BACKEND=wayland\nSDL_VIDEODRIVER=wayland\nGBM_BACKEND=nvidia-drm\n__GLX_VENDOR_LIBRARY_NAME=nvidia\nMOZ_ENABLE_WAYLAND=1\nQT_QPA_PLATFORMTHEME=qt5ct" >> $HOME/.config/environment.d/envvars.conf
-echo -e "--enable-features=UseOzonePlatform,WaylandWindowDecorations\n--ozone-platform=wayland\n--no-sandbox\n--disable-gpu" >> $HOME/.config/electron-flags.conf
+echo -e "--enable-features=UseOzonePlatform,WaylandWindowDecorations\n--ozone-platform=wayland\n--no-sandbox">> $HOME/.config/electron-flags.conf
 ln -s $HOME/.config/electron-flags.conf $HOME/.config/electron-16-flags.conf
 ln -s $HOME/.config/electron-flags.conf $HOME/.config/electron17-flags.conf
 ln -s $HOME/.config/electron-flags.conf $HOME/.config/code-flags.conf
@@ -47,7 +47,7 @@ makepkg -si --noconfirm
 rm -rf $HOME/gitrepos
 
 ## AUR Packages
-yay -S --needed --noconfirm papirus-libreoffice-theme minigalaxy itch-setup-bin minecraft-launcher mangohud noisetorch extramaus downgrade rider visual-studio-code-insiders-bin ttf-google-sans pfetch papirus-libreoffice-theme gnome-text-editor gnome-shell-extension-pop-shell-git chrome-gnome-shell adw-gtk3 gnome-shell-extension-openweather-git nerd-fonts-jetbrains-mono
+yay -S --needed --noconfirm papirus-libreoffice-theme minigalaxy itch-setup-bin polymc mangohud noisetorch extramaus downgrade rider visual-studio-code-insiders-bin ttf-google-sans pfetch papirus-libreoffice-theme gnome-text-editor gnome-shell-extension-pop-shell-git chrome-gnome-shell adw-gtk3 gnome-shell-extension-openweather-git nerd-fonts-jetbrains-mono
 echo "Run hp-setup [ip address of printer] to set up wireless printer"
 echo "Authenticate github using github cli"
 echo "Set up noisetorch systemd unit"
