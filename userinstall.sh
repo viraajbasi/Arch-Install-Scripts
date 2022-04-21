17534d92ec84d3e56718c1d3bd682b7dea14dfc2
Viraaj Basi <viraajbasi@gmail.com>
2022-04-22 00:37:40 +0100

    Updated programs

--- userinstall.sh
+++ userinstall.sh
@@ -47,7 +47,7 @@ makepkg -si --noconfirm
 rm -rf $HOME/gitrepos
 
 ## AUR Packages
-yay -S --needed --noconfirm papirus-libreoffice-theme minigalaxy itch-setup-bin polymc mangohud noisetorch extramaus downgrade rider visual-studio-code-insiders-bin ttf-google-sans pfetch papirus-libreoffice-theme gnome-text-editor gnome-shell-extension-pop-shell-git chrome-gnome-shell adw-gtk3 gnome-shell-extension-openweather-git nerd-fonts-jetbrains-mono
+yay -S --needed --noconfirm papirus-libreoffice-theme minigalaxy itch-setup-bin polymc-bin mangohud noisetorch extramaus downgrade rider visual-studio-code-insiders-bin ttf-google-sans pfetch papirus-libreoffice-theme gnome-text-editor gnome-shell-extension-pop-shell-git chrome-gnome-shell adw-gtk3 gnome-shell-extension-openweather-git nerd-fonts-jetbrains-mono
 echo "Run hp-setup [ip address of printer] to set up wireless printer"
 echo "Authenticate github using github cli"
 echo "Set up noisetorch systemd unit"
